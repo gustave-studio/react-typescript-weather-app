3c9076d874197a4f0b2ab7fecadf81c4262af817
gustave-studio <gustave0222@gmail.com>
2022-02-03 15:27:26 +0900

    ✨結果の型を作成

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,8 +1,18 @@
 import { useState } from 'react';
 
-const Results = () => {
+type ResultsPropsType = {
+  results: {
+    country: string;
+    cityName: string;
+    temperature: string;
+    conditionText: string;
+    icon: string;
+  }
+}
+
+const Results = (props: ResultsPropsType) => {
   return(
-    <h1>気象データ</h1>
+    <h1>気象データ{props.results.country}</h1>
   );
 };
 
