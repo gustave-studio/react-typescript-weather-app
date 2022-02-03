aa52564212d001ad2bc7612ed9c5b61147ba0f55
gustave-studio <gustave0222@gmail.com>
2022-02-03 12:19:13 +0900

    ✨APIを叩く処理追加

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
-
 const Form = () => {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    console.log(process.env.REACT_APP_WEATHER_API)
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=London&aqi=no`).then(res => res.json()).then(data => console.log(data))
+  }
+
+  
   return(
     <form>
-      <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-      {city}
+      <input type="text" name="city" placeholder="都市名" onChange={getWeather}/>
+      { city }
       <button type="submit">
         Get Weather
       </button>
